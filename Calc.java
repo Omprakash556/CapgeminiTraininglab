97290ed050c181bf3cdd18a39c93a32c7bd43379
Om prakash <116386573+Omprakash556@users.noreply.github.com>
2025-01-07 14:47:56 +0530

    Update Calc.java

--- Calc.java
+++ Calc.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 
 
-public class Calc {
+public class Calculator {
     public static void main(String[] args){
         Scanner scn=new Scanner(System.in);
         int a,b,c;
